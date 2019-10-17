2b88f237dc7a32bc0021731cfd8b762b84f3ce7e
xians78 <chr_schweiger@hotmail.com>
2019-10-17 21:43:54 +0200

    Update ShareClient.swift

--- ShareClient/ShareClient.swift
+++ ShareClient/ShareClient.swift
@@ -41,7 +41,7 @@ public enum KnownShareServers: String {
          https://github.com/dabear/NightscoutShareServer
     */
 
-    case CUSTOM="https://cgmforjeanetteshareserver.herokuapp.com"
+    case CUSTOM="https://xians78shareserver.herokuapp.com/"
 }
 
 // From the Dexcom Share iOS app, via @bewest and @shanselman:
